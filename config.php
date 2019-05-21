c9547ef57ae37e7355a1a68b667a84e07ac62ee9
swchen1217 <swchen1217@gmail.com>
2019-05-21 14:20:20 +0800

    re

--- config.php
+++ config.php
@@ -1,14 +1,15 @@
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <?php
+//header("Content-Type: text/html; charset=utf-8");
 date_default_timezone_set('Asia/Taipei');
 $mysql_host = "localhost";
 $mysql_user = "swc";
 $mysql_pass = "chen1217";
 $mysql_db   = "smartcap";
-$mysql_con = mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
-if (!$mysql_con) 
-	echo("連接錯誤: " . mysqli_connect_error()); 
+$con = mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
+if (!$con) 
+	//die("連接錯誤: " . iconv('gbk', 'utf-8//IGNORE', mysqli_connect_error())); 
+	die("連接錯誤"); 
 else
 	echo("伺服器連線成功");
-mysqli_query($mysql_con,"set names utf8");
+mysqli_query($con,"set names utf8");
 ?>
