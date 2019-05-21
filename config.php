2a0595f362f8ec6e7b5a23f9e070bb2361b487a0
swchen1217 <swchen1217@gmail.com>
2019-05-21 10:16:28 +0800

    re

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ $mysql_pass = "chen1217";
 $mysql_db   = "smartcap";
 $mysql_con = mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
 if (!$mysql_con) 
-{ 
-    echo("連接錯誤: " . mysqli_connect_error()); 
-}
-mysql_query($mysql_con,"set names utf8");
+	echo("連接錯誤: " . mysqli_connect_error()); 
+else
+	echo("伺服器連線成功");
+mysqli_query($mysql_con,"set names utf8");
 ?>
